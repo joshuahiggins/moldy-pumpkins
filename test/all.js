913fbd2d3450d1a70253391bfc1e1332a17fbcee
Joshua Higgins <joshua.higgins@toyota.com>
2018-01-31 11:04:08 -0500

    v0.1.0

---
+++ test/all.js
@@ -0,0 +1,8 @@
+const index = require('../index');
+const inspect = require('../lib/inspect');
+const Promise = require('bluebird');
+
+describe('all', () => {
+  it('should get me a list of movies', () =>
+    Promise.resolve(index.handler()).then((movies) => { inspect(movies); }));
+});
